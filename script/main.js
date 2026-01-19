891ca4350d41eefa03f72b7f7cea95b7c25f3ae4
troyHub5 <myworkchel@outlook.com>
2026-01-19 14:29:05 +0300

    swiper

--- script/main.js
+++ script/main.js
@@ -1,11 +1,17 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
+  spacebetween: 50,
+  slidePerView: 1,
+  speed: 800,
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    dynamicBullets: true,
   },
 
   
